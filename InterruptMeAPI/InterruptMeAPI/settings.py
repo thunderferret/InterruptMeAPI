1c4caa6847dab3d116e5c57f0a40eb70ad393fa1
Naruu <esara2021@gmail.com>
2019-06-26 05:22:58 +0900

    Default database to mysql

--- InterruptMeAPI/InterruptMeAPI/settings.py
+++ InterruptMeAPI/InterruptMeAPI/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'RESTapi',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +78,12 @@ WSGI_APPLICATION = 'InterruptMeAPI.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.path.join(BASE_DIR, 'mysql'),
+        'USER' : 'root',
+        'PASSWORD' : '0000',
+        'HOST' : '127.0.0.1',
+        'PORT' : '8080',
     }
 }
 
--- InterruptMeAPI/RESTapi/models.py
+++ InterruptMeAPI/RESTapi/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from __future__ import unicode_literals
+from django.db import models
+
 
 # Create your models here.
