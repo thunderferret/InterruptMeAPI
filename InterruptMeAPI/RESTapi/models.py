0299e22d1786e2de0d36f27543d191253704007d
Naruu <esara2021@gmail.com>
2019-07-01 01:14:46 -0400

    Implemented model

--- InterruptMeAPI/RESTapi/models.py
+++ InterruptMeAPI/RESTapi/models.py
@@ -4,3 +4,10 @@ from django.db import models
 
 
 # Create your models here.
+
+class User(models.Model):
+    uid = models.CharField('uid', max_length=30)
+    interval = models.IntegerField('interval')
+    start_time = models.DateTimeField('start_time')
+    end_time = models.DateTimeField('end_time')
+    isActive = models.BooleanField('isactive')
